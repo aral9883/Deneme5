09e96cdc68aa1eca7acfcf5a9ac9e065e846381e
aral <aral@192.168.1.2>
2022-11-03 18:39:38 +0300

    blaaaaa

---
+++ src/jdkdenemsi/denemee.java
@@ -0,0 +1,11 @@
+package jdkdenemsi;
+
+public class denemee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("şadiye");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author aral
+ *
+ */
+module jdkdenemsi {
+}
